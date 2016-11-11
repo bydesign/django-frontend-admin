c268476feb61416197e4e137c02650816ffba37b
Joel Leineweber <jleineweber@ur.local>
2016-11-11 10:38:05 -0500

    made overall class for scoping variables

--- js/default.js
+++ js/default.js
@@ -1,19 +1,24 @@
-console.log('getting started');
+class FrontAdmin {
+  init() {
+    console.log('initializing Front Admin');
 
-var templateList = [];
-var mainTemplate;
+    this.templateList = [];
+    this.mainTemplate;
+    var that = this;
 
-TEMPLATES.forEach(function(tempData) {
-  templateList.push(template);
-  var template = new Template(tempData.name, tempData.content, '');
-  if (tempData.start) {
-    mainTemplate = template;
+    TEMPLATES.forEach(function(tempData) {
+      that.templateList.push(template);
+      var template = new Template(tempData.name, tempData.content, '');
+      if (tempData.start) {
+        that.mainTemplate = template;
+      }
+    });
   }
-});
 
-$('#replace').click(function() {
-  var html = mainTemplate.render();
-  html = html.replace(/^.+\<body\>/,'').replace('</body>','').replace('</html>','');
-  $("body > *").not("#frontendEditor,#djDebug").remove()
-  $('body').prepend(html);
-})
+  render() {
+    var html = this.mainTemplate.render();
+    html = html.replace(/^.+\<body\>/,'').replace('</body>','').replace('</html>','');
+    $("body > *").not("#frontendEditor,#djDebug").remove()
+    $('body').prepend(html);
+  }
+}
